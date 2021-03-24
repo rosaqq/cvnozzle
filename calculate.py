ff46cf5dc8ce327a02fd1d9c474ff5c2f25a51a3
Tiago Rosa <tcrosa27@gmail.com>
2021-03-24 23:16:16 +0000

    136

--- calculate.py
+++ calculate.py
@@ -5,15 +5,17 @@ import sys
 from atm import *
 from flow import *
 
+plt_count = 0
+
 
 def main(args):
     matplotlib.use('tkagg')
-    plt_count = 0
 
     gamma = 1.4
     g = 9.8
     tmin = 0
     tmax = 200
+    steps = 100
 
     # Problem inputs
 
@@ -32,9 +34,18 @@ def main(args):
     def plot(data, label):
         global plt_count
         plt.figure(plt_count)
-        plt.plot(x, data, label=label)
+        plt.plot(x[:len(data)], data, label=label)
         plt.legend()
         plt_count += 1
+
+    def fill_zeros(array_in):
+        """
+        Fills data array to match time array for plotting
+        :param array_in:
+        :return:
+        """
+
+        return np.pad(array_in, (0, steps - len(array_in)), constant_values=0)
 
     # #######################
     # ### Calc Functions  ###
@@ -55,11 +66,11 @@ def main(args):
     def drag(height, velocity):
         a2 = exit_area
         rho = get_ambient_density(height)
-        airspeed = velocity
+        airspeed = np.abs(velocity)
         mach = airspeed / gd.sonic_velocity(temperature=get_ambient_temperature(height))
-
         coef_d = -3e-6 * np.power(mach, 6) + 0.0002 * np.power(mach, 5) - 0.0046 * np.power(mach, 4) + 0.053 * \
                  np.power(mach, 3) - 0.2806 * np.power(mach, 2) + 0.6211 * mach + 0.0568
+
         return rho * np.power(airspeed, 2) * coef_d * a2 / 2
 
     def thrust(height):
@@ -72,7 +83,9 @@ def main(args):
 
         v_exit = get_v_exit(exit_area, critical_area, stagnation_temperature, stagnation_pressure, ambient_pressure,
                             gamma)
-
+        if v_exit < 0:
+            print('shock inside, aborting')
+            return 0
         pressure_ratio_data.append(ambient_pressure / stagnation_pressure)
 
         if mass_flow == 0:
@@ -88,7 +101,7 @@ def main(args):
         # print(f'thrust: {th}, drag: {dg}, mass: {m}')
         return (th - dg) / m - g
 
-    x = np.linspace(tmin, tmax, 100)
+    x = np.linspace(tmin, tmax, steps)
     current_velocity = 0
     current_height = 0
 
@@ -109,37 +122,43 @@ def main(args):
             current_mass = initial_mass - mass_flux(t) * t
             # else current mass keeps def
 
-        if current_height >= 0:
-            # accel instant
-            accel = get_accel(current_height, current_velocity, current_mass)
-            # print(accel)
-            dv = accel * (t - prev_ts)
-            current_velocity += dv
-        else:
-            accel = 0
-            current_velocity = 0
+        # accel instant
+        accel = get_accel(current_height, current_velocity, current_mass)
+        # print(accel)
+        dv = accel * (t - prev_ts)
+        current_velocity += dv
+
+        if current_velocity < 0:
+            # print('starting to fall, stopping simulation')
+            top_height = current_height
+            break
 
         dh = current_velocity * (t - prev_ts)
         current_height += dh
 
+        # if current_height < 0:
+        #     current_height = 0
+        #     accel = 0
+        #     current_velocity = 0
+
         accel_data.append(accel)
         # print(f'accel_data: {accel_data}, curr_accel: {accel}')
         vel_data.append(current_velocity)
-        # print(f' vel_data: {vel_data}, curr_vel: {curr_velocity}')
+        # print(f' vel_data: {vel_data}, curr_vel: {current_velocity}')
         height_data.append(current_height)
-        if current_height > top_height:
-            top_height = current_height
         # print(f' height_data: {height_data}, curr_height: {current_height}')
 
         prev_ts = t
 
+    # plot(fill_zeros(accel_data), 'accel')
+    # plot(fill_zeros(vel_data), 'vel')
+    # plot(fill_zeros(height_data), 'height')
+    # plot(fill_zeros(thrust_data), 'thrust')
+
     # plot(accel_data, 'accel')
     # plot(vel_data, 'vel')
     # plot(height_data, 'height')
-    # arr = np.pad(pressure_ratio_data, (0, 100 - len(pressure_ratio_data)), constant_values=0)
-    # plot(arr, 'pb/pc')
-    # arr2 = np.pad(thrust_data, (0, 100 - len(thrust_data)), constant_values=0)
-    # plot(arr2, 'thrust')
+    # plot(thrust_data, 'thrust')
     # plt.show()
 
     # print(f'top height: {top_height}, reached at t = {x[height_data.index(top_height)]}')
@@ -147,4 +166,5 @@ def main(args):
 
 
 if __name__ == '__main__':
-    print(main(sys.argv))
+    # print(main(sys.argv))
+    print(main(['', 0.1562, 1.3673, 5959.183673469387]))
