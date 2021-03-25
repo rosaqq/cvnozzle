f09edb93e2ae95126afd63a2e3ad4c4ccfeebc21
Tiago Rosa <tcrosa27@gmail.com>
2021-03-25 01:00:52 +0000

    working rocket class

--- calculate.py
+++ calculate.py
@@ -1,8 +1,8 @@
 import matplotlib
 import matplotlib.pyplot as plt
 
-from atm import *
 from flow import *
+from rocket import Rocket
 
 plt_count = 0
 
@@ -13,8 +13,6 @@ def main(args=None):
     if args is None:
         args = ['', 0.1562, 1.3673, 6000]
 
-    gamma = 1.4
-    g = 9.8
     tmin = 0
     tmax = 200
     steps = 200
@@ -24,153 +22,30 @@ def main(args=None):
     # a / a* -> ex / crit
     critical_area = float(args[1])
     a_ratio = float(args[2])
-    exit_area = critical_area * a_ratio
 
     stagnation_pressure = 7e6
     stagnation_temperature = 2500
     initial_mass = float(args[3])
 
-    # atmospheric formulas
-    sea_level_pressure = 101325  # Pa -> N/m^2
-
     def plot(data, label):
         global plt_count
         plt.figure(plt_count)
-        plt.plot(x[:len(data)], data, label=label)
+        plt.plot(np.linspace(tmin, tmax, steps), data, label=label)
         plt.legend()
         plt_count += 1
 
-    def fill_zeros(array_in):
-        """
-        Fills data array to match time array for plotting
-        :param array_in:
-        :return:
-        """
+    rocket = Rocket(stagnation_pressure, stagnation_temperature, critical_area, a_ratio, initial_mass)
+    rocket.launch(tmin, tmax, steps)
 
-        return np.pad(array_in, (0, steps - len(array_in)), constant_values=0)
-
-    # #######################
-    # ### Calc Functions  ###
-    # #######################
-
-    # ------ Rocket Functions # todo: rocket class
-    # ----------------------------------------------------------------------------------------------------------------------
-    def mass_flux(time):
-        # m_flow is constant for a specified nozzle geometry
-        # m_flow depends on throat_area
-        flux = gd.mass_flux_max(stagnation_pressure, stagnation_temperature) * critical_area
-        # kg/m^2
-
-        if flux * time >= 0.6 * initial_mass:
-            return 0
-        return flux
-
-    def drag(height, velocity):
-        a2 = exit_area
-        rho = get_ambient_density(height)
-        airspeed = np.abs(velocity)
-        mach = airspeed / gd.sonic_velocity(temperature=get_ambient_temperature(height))
-        coef_d = -3e-6 * np.power(mach, 6) + 0.0002 * np.power(mach, 5) - 0.0046 * np.power(mach, 4) + 0.053 * \
-                 np.power(mach, 3) - 0.2806 * np.power(mach, 2) + 0.6211 * mach + 0.0568
-
-        return rho * np.power(airspeed, 2) * coef_d * a2 / 2
-
-    def thrust(height):
-        exit_mach = get_exit_mach(exit_area, critical_area)
-        mass_flow = mass_flux(t)
-        exit_pressure = stagnation_pressure * np.power(1 + (gamma - 1) / 2 * np.power(exit_mach, 2),
-                                                       -gamma / (gamma - 1))
-
-        ambient_pressure = get_ambient_pressure(height)
-
-        v_exit = get_v_exit(exit_area, critical_area, stagnation_temperature, stagnation_pressure, ambient_pressure,
-                            gamma)
-        if v_exit < 0:
-            print('shock inside, aborting')
-            return 0
-        pressure_ratio_data.append(ambient_pressure / stagnation_pressure)
-
-        if mass_flow == 0:
-            return 0
-        return mass_flow * v_exit + exit_pressure * exit_area - ambient_pressure * exit_area
-
-    def get_accel(height, vel, mass):
-        th = thrust(height)
-        thrust_data.append(th)
-        dg = drag(height, vel)
-        dg_data.append(dg)
-        m = mass
-
-        # print(f'thrust: {th}, drag: {dg}, mass: {m}')
-        return (th - dg) / m - g
-
-    x = np.linspace(tmin, tmax, steps)
-    current_velocity = 0
-    current_height = 0
-
-    accel_data = []
-    vel_data = []
-    height_data = []
-    top_height = 0
-
-    pressure_ratio_data = []
-    thrust_data = []
-    dg_data = []
-
-    prev_ts = 0
-
-    current_mass = initial_mass
-
-    for t in x:
-        if mass_flux(t) > 0:
-            current_mass = initial_mass - mass_flux(t) * t
-            # else current mass keeps def
-
-        # accel instant
-        accel = get_accel(current_height, current_velocity, current_mass)
-        # print(accel)
-        dv = accel * (t - prev_ts)
-        current_velocity += dv
-
-        # if current_velocity < 0:
-        #     # print('starting to fall, stopping simulation')
-        #     top_height = current_height
-        #     break
-
-        dh = current_velocity * (t - prev_ts)
-        current_height += dh
-
-        if current_height < 0:
-            current_height = 0
-            accel = 0
-            current_velocity = 0
-
-        accel_data.append(accel)
-        # print(f'accel_data: {accel_data}, curr_accel: {accel}')
-        vel_data.append(current_velocity)
-        # print(f' vel_data: {vel_data}, curr_vel: {current_velocity}')
-        height_data.append(current_height)
-        # print(f' height_data: {height_data}, curr_height: {current_height}')
-
-        if current_height > top_height:
-            top_height = current_height
-
-        prev_ts = t
-
-    # plot(fill_zeros(accel_data), 'accel')
-    # plot(fill_zeros(vel_data), 'vel')
-    # plot(fill_zeros(height_data), 'height')
-    # plot(fill_zeros(thrust_data), 'thrust')
-
-    plot(accel_data, 'accel')
-    plot(vel_data, 'vel')
-    plot(height_data, 'height')
-    plot(thrust_data, 'thrust')
-    plot(dg_data, 'drag')
+    plot(rocket.accel_data, 'accel')
+    plot(rocket.vel_data, 'vel')
+    plot(rocket.height_data, 'height')
+    plot(rocket.thrust_data, 'thrust')
+    plot(rocket.drag_data, 'drag')
     plt.show()
 
     # print(f'top height: {top_height}, reached at t = {x[height_data.index(top_height)]}')
-    return top_height, x[height_data.index(top_height)]
+    return rocket.top_height
 
 
 if __name__ == '__main__':
---
+++ rocket.py
@@ -0,0 +1,119 @@
+from atm import *
+from flow import *
+
+
+class Rocket:
+
+    def __init__(self, stagnation_pressure, stagnation_temperature, critical_area, area_ratio, initial_mass):
+        self.stagnation_pressure = stagnation_pressure
+        self.stagnation_temperature = stagnation_temperature
+        self.critical_area = critical_area
+        self.area_ratio = area_ratio
+        self.exit_area = critical_area * area_ratio
+        self.initial_mass = initial_mass
+
+        # constants
+        self.gamma = 1.4
+        self.g = 9.8
+
+        self.current_accel = 0
+        self.current_vel = 0
+        self.current_height = 0
+        self.top_height = 0
+
+        self.accel_data = []
+        self.vel_data = []
+        self.height_data = []
+        self.thrust_data = []
+        self.drag_data = []
+
+    def mass_flux(self, time):
+        # m_flow is constant for a specified nozzle geometry
+        # m_flow depends on throat_area
+        flux = gd.mass_flux_max(self.stagnation_pressure, self.stagnation_temperature) * self.critical_area
+        # kg/m^2
+
+        if flux * time >= 0.6 * self.initial_mass:
+            return 0
+        return flux
+
+    def drag(self, height, velocity):
+        a2 = self.exit_area
+        rho = get_ambient_density(height)
+        airspeed = np.abs(velocity)
+        mach = airspeed / gd.sonic_velocity(temperature=get_ambient_temperature(height))
+        coef_d = -3e-6 * np.power(mach, 6) + 0.0002 * np.power(mach, 5) - 0.0046 * np.power(mach, 4) + 0.053 * \
+            np.power(mach, 3) - 0.2806 * np.power(mach, 2) + 0.6211 * mach + 0.0568
+
+        return rho * np.power(airspeed, 2) * coef_d * a2 / 2
+
+    def thrust(self, t, height):
+        exit_mach = get_exit_mach(self.exit_area, self.critical_area)
+        mass_flow = self.mass_flux(t)
+        gamma = self.gamma
+
+        exit_pressure = self.stagnation_pressure * np.power(1 + (gamma - 1) / 2 * np.power(exit_mach, 2),
+                                                            -gamma / (gamma - 1))
+
+        ambient_pressure = get_ambient_pressure(height)
+
+        v_exit = get_v_exit(self.exit_area, self.critical_area, self.stagnation_temperature, self.stagnation_pressure, ambient_pressure,
+                            gamma)
+        if v_exit < 0:
+            print('shock inside, aborting')
+            return 0
+
+        if mass_flow == 0:
+            return 0
+        return mass_flow * v_exit + exit_pressure * self.exit_area - ambient_pressure * self.exit_area
+
+    def get_accel(self, t, height, vel):
+        if self.mass_flux(t) > 0:
+            mass = self.initial_mass - self.mass_flux(t) * t
+        else:
+            mass = 0.6*self.initial_mass
+
+        th = self.thrust(t, height)
+        self.thrust_data.append(th)
+        dg = self.drag(height, vel)
+        self.drag_data.append(dg)
+        m = mass
+
+        # print(f'thrust: {th}, drag: {dg}, mass: {m}')
+        return (th - dg) / m - self.g
+
+    def launch(self, tmin, tmax, steps):
+        x = np.linspace(tmin, tmax, steps)
+        prev_ts = 0
+        for t in x:
+
+            # accel instant
+            self.current_accel = self.get_accel(t, self.current_height, self.current_vel)
+            # print(accel)
+            dv = self.current_accel * (t - prev_ts)
+            self.current_vel += dv
+
+            # if current_velocity < 0:
+            #     # print('starting to fall, stopping simulation')
+            #     top_height = current_height
+            #     break
+
+            dh = self.current_vel * (t - prev_ts)
+            self.current_height += dh
+
+            if self.current_height < 0:
+                self.current_accel = 0
+                self.current_vel = 0
+                self.current_height = 0
+
+            self.accel_data.append(self.current_accel)
+            # print(f'accel_data: {accel_data}, curr_accel: {accel}')
+            self.vel_data.append(self.current_vel)
+            # print(f' vel_data: {vel_data}, curr_vel: {current_velocity}')
+            self.height_data.append(self.current_height)
+            # print(f' height_data: {height_data}, curr_height: {current_height}')
+
+            if self.current_height > self.top_height:
+                self.top_height = self.current_height
+
+            prev_ts = t
